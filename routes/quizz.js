84c6b2f392fa2bf4081a0dfec6b47c6c4f4359e2
meriam.abid@esprit.tn <ikramhammel44@gmail.com>
2024-05-02 01:53:17 +0100

    avn

--- routes/quizz.js
+++ routes/quizz.js
@@ -7,6 +7,9 @@ router.get('/showall', quizzController.getAllQuizz);
 router.delete('/delete/:id', quizzController.deleteQuizz);
 router.get('/show/:id', quizzController.getQuizz);
 router.put('/update/:id', quizzController.modifyQuizz);
+router.post('/get/:id',quizzController.getQuestions);
+router.get('/similar',quizzController.getSimilaires);
+
 
 router.post('/qrcode', quizzController.QrCode);
 module.exports = router;
